17b159398b95ddf83f65f66c3be2130316095c1e
Riki-Toro <rikitoro22@gmail.com>
2021-09-10 18:11:11 +0700

    menambahkan file judul

---
+++ src/Judul.js
@@ -0,0 +1,13 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Judul extends Component {
+    render() {
+        return (
+            <div>
+                <h1>List menggunakan API</h1>
+            </div>
+        )
+    }
+}
+
+export default Judul;
