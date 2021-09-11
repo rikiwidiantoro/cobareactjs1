330fadda6d4ee5ff1b214c9bf618ce0995244569
Riki-Toro <rikitoro22@gmail.com>
2021-09-11 18:36:02 +0700

    class footer

---
+++ src/komponen/Footer.js
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return(
+            <div>
+                <p><strong>&copy;</strong> Riki Widiantoro 2021</p>
+            </div>
+        )
+    }
+}
